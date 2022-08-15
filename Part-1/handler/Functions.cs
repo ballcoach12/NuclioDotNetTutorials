588a51d5c38701b25e8705e183895a747d70d465
ballcoach <integragreg@gmail.com>
2022-08-15 11:10:44 -0400

    Restructured the project files so that we can end up with only a main branch after PR.

---
+++ Part-1/handler/Functions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Nuclio.Sdk;
+
+public class Function
+{
+    public object execute(Context context, Event eventBase)
+    {
+        return new Response()
+        {
+            StatusCode = 200,
+            ContentType = "application/text",
+            Body = ""
+        };
+    }
+}
---
+++ Part-2/handler/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Nuclio.Sdk;
+
+// @nuclio.configure
+//
+// function.yaml
+//   spec:
+//     runtime: dotnetcore
+//     handler: Function:execute
+public class function
+{
+    public object execute(Context context, Event eventBase)
+    {
+        var body = eventBase.GetBody().ToString();
+        return new Response()
+        {
+            StatusCode = 200,
+            ContentType = "application/text",
+            Body = "Hello! I executed! You sent the following body contents:" + body
+        };
+    }
+}
